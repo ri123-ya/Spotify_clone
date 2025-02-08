225a2d2b37ffcfa97c613be0315e03dfc25d90e6
Riya Rastogi <riya02rastogi@gmail.com>
2025-02-08 19:19:50 +0530

    setting music at client side

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
- 
+console.log('Lets write JavaScript');
+
+ async function getSongs() {
+   
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href)
+        }
+    }
+    return songs;
+ }
+
+ async function main(){ 
+
+    let songs = await getSongs()
+    console.log(songs)
+     
+    
+    var audio = new Audio(songs[0]);
+    audio.play(); // Play the audio when the button is clicked
+   
+
+   audio.addEventListener("loadeddata", () => {
+        let duration = audio.duration;
+        console.log(duration)
+    });
+ } 
+   
+ main()
