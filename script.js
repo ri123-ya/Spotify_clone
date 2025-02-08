14622ecb8697e50621f0a9f27f78f7acee1e7f3d
Riya Rastogi <riya02rastogi@gmail.com>
2025-02-08 21:08:23 +0530

    adding music content

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
 console.log('Lets write JavaScript');
 
- async function getSongs() {
-   
-    let a = await fetch("http://127.0.0.1:3000/songs/")
+async function getSongs(folder) {
+
+    currFolder = folder;
+    let a = await fetch(`/${folder}/`)
     let response = await a.text();
-    console.log(response)
     let div = document.createElement("div")
     div.innerHTML = response;
     let as = div.getElementsByTagName("a")
-    let songs = []
+    songs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href)
+            songs.push(element.href.split(`/${folder}/`)[1])
         }
     }
     return songs;
- }
 
- async function main(){ 
 
-    let songs = await getSongs()
-    console.log(songs)
-     
-    
+
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    songUL.innerHTML = ""
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" width="34" src="img/music.svg" alt="">
+                            <div class="info">
+                                <div> ${song.replaceAll("%20", " ")}</div>
+                                <div>Harry</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="img/play.svg" alt="">
+                            </div> </li>`;
+    }
+
+
+
     var audio = new Audio(songs[0]);
     audio.play(); // Play the audio when the button is clicked
-   
 
-   audio.addEventListener("loadeddata", () => {
-        console.log(audio.duration,audio.currentSrc,audio.currentTime )
+
+    audio.addEventListener("loadeddata", () => {
+        console.log(audio.duration, audio.currentSrc, audio.currentTime)
     });
- }  
-   
-  
- main()
+}
+
+
+main()
