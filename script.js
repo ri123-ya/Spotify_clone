343580206a442f6787a1d8d80237c4a0cbc1b22a
Riya Rastogi <riya02rastogi@gmail.com>
2025-02-09 05:35:21 +0530

    adding seekbar

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 console.log('Lets write JavaScript');
 
 async function getSongs(folder) {
-
     currFolder = folder;
     let a = await fetch(`/${folder}/`)
     let response = await a.text();
@@ -15,17 +14,17 @@ async function getSongs(folder) {
             songs.push(element.href.split(`/${folder}/`)[1])
         }
     }
-    return songs;
-}
+ 
 
 
-async function main() {
+    // Show all the songs in the playlist
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    songUL.innerHTML = ""
     for (const song of songs) {
         songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" width="34" src="img/music.svg" alt="">
                             <div class="info">
                                 <div> ${song.replaceAll("%20", " ")}</div>
-                                <div>Riya</div>
+                                <div>Singer</div>
                             </div>
                             <div class="playnow">
                                 <span>Play Now</span>
@@ -33,11 +32,15 @@ async function main() {
                             </div> </li>`;
     }
 
+    // Attach an event listener to each song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
 
-    audio.addEventListener("loadeddata", () => {
-        console.log(audio.duration, audio.currentSrc, audio.currentTime)
-    });
+        })
+    })
+
+    return songs
 }
-
 
 main()
