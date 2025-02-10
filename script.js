69e78804f6297714d0ba1e75d730f0397055fbc1
Riya Rastogi <riya02rastogi@gmail.com>
2025-02-10 09:02:27 +0530

    adding close button in left

--- script.js
+++ script.js
@@ -97,5 +97,16 @@ async function main() {
         document.querySelector(".circle").style.left = percent + "%";
         currentSong.currentTime = ((currentSong.duration) * percent) / 100
     })
+
+    // Add an event listener for hamburger
+    document.querySelector(".hamburger").addEventListener("click", () => {
+        document.querySelector(".left").style.left = "0"
+    })
+
+    // Add an event listener for close button
+    document.querySelector(".close").addEventListener("click", () => {
+        document.querySelector(".left").style.left = "-120%"
+    })
+ 
 }
  main()
